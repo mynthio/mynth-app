4316d5aab76fd21b8c9edb3134d9346105d38a6c
Tomasz Szczuciński <tomasz.szczucinski@mynth.io>
2024-12-03 23:15:01 +0100

    feat: update migrations

--- src-tauri/migrations/001_create_initial_tables.sql
+++ src-tauri/migrations/001_create_initial_tables.sql
@@ -1,12 +1,45 @@
-CREATE TABLE IF NOT EXISTS chats (
-    id TEXT PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS messages (
-    id TEXT PRIMARY KEY,
-    chat_id TEXT,
-    content TEXT,
+CREATE TABLE chats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    current_branch_id INTEGER,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (chat_id) REFERENCES chats(id)
-); 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE branches (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    latest_message_id INTEGER,
+    chat_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chat_id) REFERENCES chats(id) ON DELETE CASCADE
+);
+CREATE TABLE messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT NOT NULL,
+    role TEXT NOT NULL,
+    branch_id INTEGER NOT NULL,
+    parent_id INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (branch_id) REFERENCES branches(id) ON DELETE CASCADE,
+    FOREIGN KEY (parent_id) REFERENCES messages(id) ON DELETE CASCADE
+);
+CREATE TABLE ai_integrations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    base_url TEXT NOT NULL,
+    api_key TEXT NOT NULL
+);
+CREATE TABLE ai_models (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    model_id TEXT NOT NULL,
+    mynth_model_id TEXT,
+    integration_id INTEGER NOT NULL,
+    FOREIGN KEY (integration_id) REFERENCES ai_integrations(id) ON DELETE CASCADE
+);
+ALTER TABLE branches
+ADD FOREIGN KEY (latest_message_id) REFERENCES messages(id) ON DELETE
+SET NULL;
+ALTER TABLE chats
+ADD FOREIGN KEY (current_branch_id) REFERENCES branches(id) ON DELETE
+SET NULL;
